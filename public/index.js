356cec6cc22e93b02e5310ef907a95da61a9d0be
Halim <book1001.db@gmail.com>
2025-07-29 00:24:10 -0400

    cache

--- public/index.js
+++ public/index.js
@@ -1,41 +1,41 @@
 // ==================================================================
 // 캐시
 // ==================================================================
-// const CACHE_NAME = "music-player-cache-v1";
-// const urlsToCache = [
-//   "/",
-//   "/index.html",
-//   "/manifest.json",
-//   "/index.css",
-//   "/index.js",
-//   // 필요한 기타 정적 자원들
-// ];
+const CACHE_NAME = "music-player-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/index.css",
+  "/index.js",
+  // 필요한 기타 정적 자원들
+];
 
-// // 설치 시 캐시 저장
-// self.addEventListener("install", event => {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-//   );
-// });
+// 설치 시 캐시 저장
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
 
-// // 활성화 시 이전 캐시 제거
-// self.addEventListener("activate", event => {
-//   event.waitUntil(
-//     caches.keys().then(keys =>
-//       Promise.all(keys.filter(key => key !== CACHE_NAME)
-//       .map(key => caches.delete(key)))
-//     )
-//   );
-// });
+// 활성화 시 이전 캐시 제거
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME)
+      .map(key => caches.delete(key)))
+    )
+  );
+});
 
-// // 네트워크 요청 가로채기 (캐시 우선 전략)
-// self.addEventListener("fetch", event => {
-//   event.respondWith(
-//     caches.match(event.request).then(response => {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
+// 네트워크 요청 가로채기 (캐시 우선 전략)
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
 // ==================================================================
 // Playlist
